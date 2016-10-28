11d35e686dec40b047496bbed700cabda2f39178
erickloulou <erickrabarijaona@gmail.com>
2016-10-28 14:51:24 +0200

    Modification méthode roll Nouveaux tests sur score

--- fr.uvsq.coo.ex3_2/src/main/java/Game.java
+++ fr.uvsq.coo.ex3_2/src/main/java/Game.java
@@ -1,13 +1,16 @@
 /**
  * Created by erickloulou on 28/10/2016.
  */
-
+import java.util.ArrayList;
 
 public class Game {
+    private int score = 0;
     public void roll(int pins){
+        score += pins;
+
 
     }
     public int score(){
-        return 0;
+        return score;
     }
 }
--- fr.uvsq.coo.ex3_2/src/test/java/GameTest.java
+++ fr.uvsq.coo.ex3_2/src/test/java/GameTest.java
@@ -4,13 +4,26 @@
 
 import junit.framework.TestCase;
 
-public class GameTest extends TestCase {
-    public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++){
-            g.roll(0);
-        }
-        assertEquals(0,g.score());
 
+public class GameTest extends TestCase {
+    private Game g;
+
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
+    private void rollMany(int n, int pins){
+        for(int i=0;i<n;i++){
+            g.roll(pins);
+        }
+    }
+    public void testGutterGame() throws Exception {
+        rollMany(20,0);
+        assertEquals(0,g.score());
+    }
+
+    public void testAllOnes() throws Exception {
+        rollMany(20,1);
+        assertEquals(20,g.score());
     }
 }
